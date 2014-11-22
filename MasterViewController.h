1f4eab6daced007944da92439aad6be719f8c142
Wade Sellers <Waderoni@gmail.com>
2014-11-22 13:14:08 -0600

    Initial Commit

---
+++ MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  ScaryBugsMac
+//
+//  Created by Wade Sellers on 11/22/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MasterViewController : NSViewController
+
+@end
---
+++ MasterViewController.m
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.m
+//  ScaryBugsMac
+//
+//  Created by Wade Sellers on 11/22/14.
+//  Copyright (c) 2014 Wade Sellers. All rights reserved.
+//
+
+#import "MasterViewController.h"
+
+@interface MasterViewController ()
+
+@end
+
+@implementation MasterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
